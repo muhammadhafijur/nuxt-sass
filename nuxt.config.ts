6d46f7b8622f8bc3993510e2de13f5269214cc69
Muhammad Hafijur <85216180+muhammadhafijur@users.noreply.github.com>
2024-01-09 12:34:55 +0600

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: [process.env.NUXT_UI_PRO_PATH || '@nuxt/ui-pro'],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxtjs/fontaine',
+    '@nuxtjs/google-fonts',
+    '@vueuse/nuxt',
+    'nuxt-og-image',
+  ],
+  ui: {
+    icons: ['heroicons', 'simple-icons']
+  },
+  // Fonts
+  fontMetrics: {
+    fonts: ['DM Sans']
+  },
+  googleFonts: {
+    display: 'swap',
+    download: true,
+    families: {
+      'DM+Sans': [300, 400, 500, 600, 700]
+    }
+  },
+  routeRules: {
+    '/api/search.json': { prerender: true },
+    '/docs': { redirect: '/docs/getting-started', prerender: false }
+  },
+  devtools: { enabled: true }
+})
